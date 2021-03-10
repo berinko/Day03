284e9a34c7969b60b2d97c18644771eb4104d917
berinko <berin@live.com>
2021-03-10 21:11:49 +0100

    zadatak drugi

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    //  ovo je neki kom
+
+    /* ovo je neki komentarovo je neki komentarovo je neki komentar
+    ovo je neki komentarovo je neki komentarovo je neki komentar
+    ovo je neki komentarovo je neki komentarovo je neki komentar*/
+
+
+}
+
+console.log (main());
+      
+
+module.exports = main;
