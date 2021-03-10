500e72fc644e040f23f0fe6f817f635bbb957d8d
berinko <berin@live.com>
2021-03-10 23:06:41 +0100

    zadatak sedmi

---
+++ ex07/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+var studlyCapVar = 10;
+var properCamelCase = "A string";
+var titleCaseOver = 9000;  
+
+
+return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver
+
+}
+}
+
+console.log (main());
+module.exports = main;
